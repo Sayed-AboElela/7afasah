be336853409d5576ea474ff6ebd2c577a4b1e87a
mohamed Swelam <wd.swelam@gmail.com>
2019-01-13 17:42:11 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -4,8 +4,22 @@ $(document).ready(function() {
             scrollTop: $("#vissionTop").offset().top
         }, 2000);
     });
+
+
+
+
 });
 
 
 
 
+function openSideNav(){
+    $('.left-links.xs-NavFix').addClass('sideOPend');
+    $('.dvOverBlack').addClass('overOpend');
+}
+
+function closeSideNav(){
+    $('.left-links.xs-NavFix').removeClass('sideOPend');
+    $('.dvOverBlack').removeClass('overOpend');
+
+}
