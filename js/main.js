7d9e4c1f8102cf0f9941ac4d2f5752feda7fbaf1
mohamed Swelam <wd.swelam@gmail.com>
2019-01-14 15:53:03 +0200

    full index

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
+    
+    $(".dirBtnTo-index").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#dirBtnTo-index").offset().top
+        }, 2000);
+    });
+    
+    $(".dirBtnTo-development").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#dirBtnTo-development").offset().top
+        }, 2000);
+    });
+    
+    $(".dirBtnTo-studios").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#dirBtnTo-studios").offset().top
+        }, 2000);
+    });
+    
+    $(".dirBtnTo-social").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#dirBtnTo-social").offset().top
+        }, 2000);
+    });
+    
+    $(".dirBtnTo-ourService").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#dirBtnTo-ourService").offset().top
+        }, 2000);
+    });
+    
     $("#prdBtn").click(function() {
         $('html, body').animate({
             scrollTop: $("#vissionTop").offset().top
